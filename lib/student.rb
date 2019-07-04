4a6b1c4ff3969caadb6c0688c5cf2e29d4fc469b
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-04 20:06:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,13 @@
 require_relative "../config/environment.rb"
+require 'pry'
 
 class Student
+  
     attr_accessor :name, :grade, :id 
 
     def initialize(name,grade, id=nil)
       @name = name 
       @grade = grade 
-      
     end 
 
     def self.create_table 
@@ -27,9 +28,9 @@ class Student
       if self.id 
         self.update
       end 
-      sql = "INSERT INTO students (name,grade) VALUES (?,?)"
-      DB[:conn].execute(sql,self.name,self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        sql = "INSERT INTO students (name,grade) VALUES (?,?)"
+        DB[:conn].execute(sql,self.name,self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end 
 
     def update 
@@ -43,8 +44,17 @@ class Student
     end 
     
     def self.new_from_db(row)
-      sql = "INSERT INTO students (name, grade) VALUES (?,?)"
-      row = Student.new(:name, :grade)
-      DB[:conn].execute(row)
+      new_student = Student.new(:name, :grade, :id) 
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
+      new_student
     end 
+    
+    def self.find_by_name(name)
+      sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+      DB[:conn].execute(sql,name).map do |row|
+        self.new_from_db(row)
+      end.first  
+    end
 end

90884173a6cd8650bd69fe0d1a8f8d3f1ddbbe39
Shawn <shawntennity@gmail.com>
2019-07-20 17:05:13 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ class Student
       DB[:conn].execute(sql,name).map do |row|
         self.new_from_db(row)
       end.first
-    
+    # This works as well. 
     #self.new_from_db(DB[:conn].execute(sql, name).first)
   end
 end

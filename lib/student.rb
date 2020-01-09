2ee36a9c033f1a9d3b2890570b17cedaf7d5af0f
Richard Lee <richlee81192@gmail.com>
2020-01-09 20:26:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -50,11 +50,10 @@ def self.create(name, grade)
 end 
 
 def self.new_from_db(row)
-  student = self.new
-  student.id = row[0]
-  student.name = row[1]
-  student.grade = row[2]
-  student
+  id = row[0]
+  name = row[1]
+  grade = row[2]
+  student = self.new(name, grade, id)
 end
 
 def self.find_by_name(name)

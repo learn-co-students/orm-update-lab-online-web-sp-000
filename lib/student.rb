fe2c75ffedf45da2ddf3fd7f3ee6885d8ebebba1
imjoshellis <joshe523@gmail.com>
2020-03-09 16:08:46 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade)
+  def initialize(id = nil, name, grade)
     @name = name
     @grade = grade
-    @id = nil
+    @id = id
   end
 
   def self.create_table
@@ -45,5 +45,29 @@ class Student
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
+
+  def self.create(name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new(row[0],row[1],row[2])
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end

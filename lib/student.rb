5d54cf792fc4d30e9bb1bcd9827f219cdbdb2d7a
Mark Harvey <harvey@DevBookMH.local>
2020-03-18 15:34:09 -0400

    added ini, cre,sv,up

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,46 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :id, :name, :grade
+
+  def initialize (id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id Integer Primary Key,
+      name Text,
+      grade Text
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+    Insert Into students (name, grade)
+    Values (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("Select last_insert_rowid() from students")[0][0]
+  end
+end
+
+  def update
+    sql = "Update students Set name = ?, grade = ? Where id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end

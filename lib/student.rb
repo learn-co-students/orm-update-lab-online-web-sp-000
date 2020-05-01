a82e9114bdeb457ce5e523446f2347a77133023d
ArslanInfinite <ali.arslan319@gmail.com>
2020-04-30 22:45:21 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,10 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class with DB[:conn]
 
-  def initialize (name, grade, id = nil)
+  def initialize (id = nil, name, grade)
+    @id = id
     @name = name 
     @grade = grade 
-    @id = id
   end 
 
   def self.create_table 

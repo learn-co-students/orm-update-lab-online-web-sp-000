e0c5512ab9874d4cb7955dad1237df6939640e1f
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-16 15:43:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -29,11 +29,39 @@ class Student
   end
   
   def save
+    if self.if
+      self.update
     sql =<<-SQL
       INSERT INTO students (name, grade)
       VALUES ( ?, ?)
     SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
-
+  
+  def self.create
+    new_student.new(row(0), row(1), row(2))
+    new_student
+  end
+  
+  def self.new_from_db(row)
+    
+  end
+  
+  def self.find_by_name(name)
+    sql =<<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+    sql = "UPDATE studetns SET name = ?, grade = ?, id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 end

706f2e6adbc974253eabd414e39bf448b85eb9d1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-05-16 15:18:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,25 @@ class Student
     @name = name
     @grade = grade
   end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def drop_table
+    sql = "DROP TABLE IS EXISTS students"
+    DB[:conn].execute(sql)
+  end
+  
+  
 
 
 end

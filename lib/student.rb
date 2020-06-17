b396e1f7edabb7bd067254d66c13f3d7e989f339
Annia Forney <anniaforney@gmail.com>
2020-06-17 17:31:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,11 +7,39 @@ class Student
 
   attr_accessor :name, :grade
   attr_reader :id
-  
+
 def initialize (id=nil, name, grade)
   @id = id
   @name = name
   @grade = grade
+end
+
+def self.create_table
+  sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+end
+
+def self.drop_table
+  sql = "DROP TABLE IF EXISTS students"
+  DB[:conn].execute(sql)
+end
+
+def save
+end
+
+def self.create
+end
+
+def self.new_from_db
+end
+
+def self.find_by_name
 end
 
 
